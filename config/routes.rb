a97bbc3342a4fd2a380c82377557262251e76a7d
Robert Gutschale <robert.gutschale@gmail.com>
2014-05-16 09:35:48 +0200

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
   get 'users/new'
 
-  # get 'static_pages/home'
-  # get 'static_pages/help'
-  # get 'static_pages/about'
-  # get 'static_pages/contact'
-
-  # get 'static_pages/home' => '/home'
   root 'static_pages#home'
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
